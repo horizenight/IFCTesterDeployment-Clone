105d447d62d94742bc77799424be700d33743d51
horizenight <kshitijroodkee1@gmail.com>
2024-09-11 18:57:52 +0530

    [deploy]: re-configure vercel.json

--- Webapp/Backend/App/main.py
+++ Webapp/Backend/App/main.py
@@ -1,28 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime
-from APP.IDS.ids import router as ids_router
-
-app = FastAPI()
-
-app.include_router(ids_router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"],  
-)
-
-@app.get("/")
-async def root():
-    return {
-        "status": "API is live",
-        "timestamp": datetime.utcnow(),
-        "version": "1.0.0", 
-        "idsSchemaVersion": "//TODO"
-    }
+from App import app
 
 if __name__ == "__main__":
     import uvicorn
